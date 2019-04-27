3aca5f30aca174dfe02c5e1c75a4064d9568f325
BuildTools <unconfigured@null.spigotmc.org>
2019-04-27 04:04:03 -0600

    Scene Switch

---
+++ MoutainLandCodeCamp/Assets/Scripts/ShackInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShackInit : MonoBehaviour
+{
+    public GameObject garret;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float garstarposx = ToSingle(-1.35);
+        float garstarposy = ToSingle(-1.4);
+        Instantiate (garret, new Vector2(garstarposx, garstarposy), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private static float ToSingle(double value)
+    {
+        return (float)value;
+    }
+}
